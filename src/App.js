0590e0d8ba9d3913d75ff0e18f23c947292b2687
kasarama <magdalena.wawrzak@gmail.com>
2020-11-01 15:23:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/*
+Svar er ikke klar endnu,
+jeg arbejder på det
+
+
+
+*/
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
